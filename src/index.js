b31cfcaf0ae9a19f996dc70bb17dd72575c7d5bb
Ashfaque <shaikhashfaque220@gmail.com>
2023-10-04 14:12:07 +0530

    Changed port 5000 to 80

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use("/users", userRouter);
 app.use("/notes", noteRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
 
